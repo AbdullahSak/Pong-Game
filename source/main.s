576bd411cf0aed9fdaafba1bdbac9c326a7e687b
Abdullah Sak <83027560+AbdullahSak@users.noreply.github.com>
2023-01-31 01:58:26 +0300

    Update main.s

--- source/main.s
+++ source/main.s
@@ -1,47 +1,46 @@
 
 		AREA	demo, CODE, READONLY
-		EXPORT	__main
-		EXPORT	main
-		IMPORT	__scatterload
-		IMPORT	number0			;importing necessary images for scoreboard
-		IMPORT	number1
-		IMPORT	number2
-		IMPORT	number3
-		IMPORT	number4
-		IMPORT	number5
-		IMPORT	p
-		IMPORT	won
+		EXPORT		__main
+		EXPORT		main
+		IMPORT		__scatterload
+		IMPORT		number0			;importing necessary images for scoreboard
+		IMPORT		number1
+		IMPORT		number2
+		IMPORT		number3
+		IMPORT		number4
+		IMPORT		p
+		IMPORT		won
 		ALIGN
 		ENTRY
-__main	PROC
+__main		PROC
 		bl		__scatterload
 		ENDP
 			
-main	PROC
-		ldr 	r0, =0x40010000 ;base address for LCD
-init	BL		start			;initialising interface
-step	ldr		r7, =p1scr		;getting player 1s score to print
-		PUSH	{r7}
+main		PROC
+		ldr 		r0, =0x40010000 	;base address for LCD
+init		BL		start			;initialising interface
+step		ldr		r7, =p1scr		;getting player 1s score to print
+		PUSH		{r7}
 		BL 		scrbrd
 		ldr		r7, =p2scr
-		PUSH	{r7}
+		PUSH		{r7}
 		BL 		scrbrd
 		BL		wallref			;function for ball bouncing
 		BL		racket			;printing rackets
 		BL		isgoal			;checking goal
-		movs	r1, #1
-		str		r1, [r0, #0xc]	;refresh lcd
-		movs	r1, #2
-		str		r1, [r0, #0xc]	;clear lcd buffer
+		movs		r1, #1
+		str		r1, [r0, #0xc]		;refresh lcd
+		movs		r1, #2
+		str		r1, [r0, #0xc]		;clear lcd buffer
 		cmp		r2, #1			;goal register set in the isgoal func
 		BEQ		init			;going back to initial position
 		b		step
-stop	b		stop
+stop		b		stop
 		ENDP
 
 ;------------------------------------------------------------------
 		;initializing function to print the first interface
-start	PROC
+start		PROC
 		;initial position for the ball and rackets
 		ldr		r7, =p1cor
 		ldr		r6, =121
@@ -57,18 +56,18 @@ start	PROC
 		;drawing player 1s bar
 		ldr		r4, =121		;row register
 		
-p1brrow	str		r4, [r0]		;update the row register
+p1brrow		str		r4, [r0]		;update the row register
 
 		ldr		r5, =5			;column counter
 
-p1brcol	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, =0xffffffff		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+p1brcol		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, =0xffffffff		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r6, =10	
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	p1brcol
-		adds	r4, r4, #1
+		bne 		p1brcol
+		adds		r4, r4, #1
 		ldr		r6, =151
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		p1brrow	
@@ -76,18 +75,18 @@ p1brcol	str		r5, [r0, #0x4]	;update column register
 		;drawing player 2s bar
 		ldr		r4, =121		;row register
 		
-p2brrow	str		r4, [r0]		;update the row register
+p2brrow		str		r4, [r0]		;update the row register
 
 		ldr		r5, =308		;column counter
 
-p2brcol	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, =0xffffffff		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+p2brcol		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, =0xffffffff		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r6, =313	
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	p2brcol
-		adds	r4, r4, #1
+		bne 		p2brcol
+		adds		r4, r4, #1
 		ldr		r6, =151
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		p2brrow
@@ -95,18 +94,18 @@ p2brcol	str		r5, [r0, #0x4]	;update column register
 		;drawing ball
 		ldr		r4, =132		;row register
 		
-ballrow	str		r4, [r0]		;update the row register
+ballrow		str		r4, [r0]		;update the row register
 
 		ldr		r5, =156		;column counter
 
-ballcol	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, =0xffffffff		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+ballcol		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, =0xffffffff		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r6, =164	
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	ballcol
-		adds	r4, r4, #1
+		bne 		ballcol
+		adds		r4, r4, #1
 		ldr		r6, =140
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		ballrow
@@ -115,7 +114,7 @@ ballcol	str		r5, [r0, #0x4]	;update column register
 		ENDP
 ;-------------------------------------------------------------------------------
 		;wall reflection function for reflection dynamics of the ball
-wallref	PROC
+wallref		PROC
 		;getting ball direction
 		ldr		r7, =balldir
 		ldr		r6, [r7]
@@ -131,45 +130,45 @@ wallref	PROC
 		BEQ		ref
 		b		brow
 		
-ref		muls	r6, r2, r6		;reflect the ball
+ref		muls		r6, r2, r6		;reflect the ball
 		
 			
 		
-brow	str		r4, [r0]		;update the row register
+brow		str		r4, [r0]		;update the row register
 
-		ldr		r3, [r7, #4]	;column counter
+		ldr		r3, [r7, #4]		;column counter
 
-bcol	str		r3, [r0, #0x4]	;update column register			
-		ldr 	r2, =0xffffffff		
-		str		r2, [r0, #0x8]	;write color to pixel
-		adds	r3, r3, #1		;next column
+bcol		str		r3, [r0, #0x4]		;update column register			
+		ldr 		r2, =0xffffffff		
+		str		r2, [r0, #0x8]		;write color to pixel
+		adds		r3, r3, #1		;next column
 		ldr		r2, =8
 		ldr		r1, [r7, #4]
-		adds	r2, r2, r1
+		adds		r2, r2, r1
 		cmp		r3, r2			;check if it reached end of column of image
-		bne 	bcol
-		adds	r4, r4, #1
+		bne 		bcol
+		adds		r4, r4, #1
 		ldr		r2, =8
 		ldr		r1, [r7]
-		adds	r2, r2, r1
+		adds		r2, r2, r1
 		cmp		r4, r2			;check if it reached end of row of image
 		bne		brow	
 		
 		;correcting ball coordinates in case it went out of the bars
 		ldr		r4, [r7]
 		ldr		r3, [r7, #4]
-		adds	r4, r4, r6
-		adds	r3, r3, r5		
+		adds		r4, r4, r6
+		adds		r3, r3, r5		
 		cmp		r4, #227		;lower bar
 		BGT		botcrt
 		cmp		r4, #38			;upper bar
 		BLT		topcrt
 		b		store
-botcrt	ldr		r4, =227
+botcrt		ldr		r4, =227
 		b		store
-topcrt	ldr		r4, =38		
+topcrt		ldr		r4, =38		
 
-store	str		r4, [r7]
+store		str		r4, [r7]
 		str		r3, [r7, #4]
 		
 		;update ball direction
@@ -181,25 +180,25 @@ store	str		r4, [r7]
 		ENDP
 ;-------------------------------------------------------------------------------
 		;racket function for racket dynamics
-racket	PROC
+racket		PROC
 		;drawing racket 1
 		ldr		r7, =p1cor
 		ldr		r6, [r7]		;row counter
 		ldr		r3, [r7]		;register to hold initial row
-rac1row	str		r6, [r0]		;update the row register
+rac1row		str		r6, [r0]		;update the row register
 
 		ldr		r5, =5			;column counter
 
-rac1col	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r4, =0xffffffff		
-		str		r4, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+rac1col		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r4, =0xffffffff		
+		str		r4, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r4, =10	
 		cmp		r5, r4			;check if it reached end of column of image
-		bne 	rac1col
-		adds	r6, r6, #1
+		bne 		rac1col
+		adds		r6, r6, #1
 		ldr		r4, =30
-		adds	r4, r4, r3
+		adds		r4, r4, r3
 		cmp		r6, r4			;check if it reached end of row of image
 		bne		rac1row			
 		
@@ -209,10 +208,10 @@ rac1col	str		r5, [r0, #0x4]	;update column register
 		cmp		r6, #10			;column of front of the racket
 		bne		rac2
 		ldr		r6, [r7]
-		subs	r3, r3, #7		;upper end of the racket
+		subs		r3, r3, #7		;upper end of the racket
 		cmp		r6, r3			
 		BLT		rac2
-		adds	r4, r4, #7		;lower end of the racket
+		adds		r4, r4, #7		;lower end of the racket
 		cmp		r6, r4
 		BGT		rac2
 		
@@ -220,27 +219,27 @@ rac1col	str		r5, [r0, #0x4]	;update column register
 		ldr		r5, =-1			
 		ldr		r7, =balldir
 		ldr		r6, [r7, #4]
-		muls	r6, r5, r6
+		muls		r6, r5, r6
 		str		r6, [r7, #4]
 		
 		;drawing racket 2
-rac2	ldr		r7, =p2cor
+rac2		ldr		r7, =p2cor
 		ldr		r6, [r7]		;row counter
 		ldr		r3, [r7]		;register to hold initial row
-rac2row	str		r6, [r0]		;update the row register
+rac2row		str		r6, [r0]		;update the row register
 
 		ldr		r5, =308		;column counter
 
-rac2col	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r4, =0xffffffff		
-		str		r4, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+rac2col		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r4, =0xffffffff		
+		str		r4, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r4, =313	
 		cmp		r5, r4			;check if it reached end of column of image
-		bne 	rac2col
-		adds	r6, r6, #1
+		bne 		rac2col
+		adds		r6, r6, #1
 		ldr		r4, =30
-		adds	r4, r4, r3
+		adds		r4, r4, r3
 		cmp		r6, r4			;check if it reached end of row of image
 		bne		rac2row	
 		
@@ -251,10 +250,10 @@ rac2col	str		r5, [r0, #0x4]	;update column register
 		cmp		r6, r5
 		bne		ext
 		ldr		r6, [r7]
-		subs	r3, r3, #7		;upper end of the racket
+		subs		r3, r3, #7		;upper end of the racket
 		cmp		r6, r3
 		BLT		ext
-		adds	r4, r4, #7		;lower end of the racket
+		adds		r4, r4, #7		;lower end of the racket
 		cmp		r6, r4
 		BGT		ext
 		
@@ -262,7 +261,7 @@ rac2col	str		r5, [r0, #0x4]	;update column register
 		ldr		r5, =-1
 		ldr		r7, =balldir
 		ldr		r6, [r7, #4]
-		muls	r6, r5, r6
+		muls		r6, r5, r6
 		str		r6, [r7, #4]
 		
 		
@@ -270,22 +269,22 @@ ext		BX		LR
 		ENDP
 ;-------------------------------------------------------------------------------
 		;scoreboard function to print the scoreboard
-scrbrd	PROC
+scrbrd		PROC
 		;drawing dash for scoreboard
 		ldr		r4, =15			;row counter
 		
-strrow	str		r4, [r0]		;update the row register
+strrow		str		r4, [r0]		;update the row register
 
 		ldr		r5, =155		;column counter
 
-strclm	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, =0xffffffff		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+strclm		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, =0xffffffff		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r6, =165	
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	strclm
-		adds	r4, r4, #1
+		bne 		strclm
+		adds		r4, r4, #1
 		ldr		r6, =20
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		strrow
@@ -293,18 +292,18 @@ strclm	str		r5, [r0, #0x4]	;update column register
 		;drawing upper bar for game area
 		ldr		r4, =35			;row counter
 		
-upbrrow	str		r4, [r0]		;update the row register
+upbrrow		str		r4, [r0]		;update the row register
 
 		ldr		r5, =0			;column counter
 
-upbrcol	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, =0xffffffff		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+upbrcol		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, =0xffffffff		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r6, =320	
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	upbrcol
-		adds	r4, r4, #1
+		bne 		upbrcol
+		adds		r4, r4, #1
 		ldr		r6, =38
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		upbrrow	
@@ -312,18 +311,18 @@ upbrcol	str		r5, [r0, #0x4]	;update column register
 		;drawing lower bar for game area
 		ldr		r4, =235 		;row counter
 		
-lwbrrow	str		r4, [r0]		;update the row register
+lwbrrow		str		r4, [r0]		;update the row register
 
 		ldr		r5, =0			;column counter
 
-lwbrcol	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, =0xffffffff		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r5, r5, #1		;next column
+lwbrcol		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, =0xffffffff		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r5, r5, #1		;next column
 		ldr		r6, =320	
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	lwbrcol
-		adds	r4, r4, #1
+		bne 		lwbrcol
+		adds		r4, r4, #1
 		ldr		r6, =238
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		lwbrrow	
@@ -335,55 +334,55 @@ lwbrcol	str		r5, [r0, #0x4]	;update column register
 		cmp		r6, #0
 		BEQ		zero
 		b		znext
-zero	ldr		r4, =number0
+zero		ldr		r4, =number0
 		
-znext	cmp		r6, #1
+znext		cmp		r6, #1
 		BEQ		one
 		b		onext
 one		ldr		r4, =number1
 		
-onext	cmp		r6, #2
+onext		cmp		r6, #2
 		BEQ		two
 		b		twnext
 two		ldr		r4, =number2
 
-twnext	cmp		r6, #3
+twnext		cmp		r6, #3
 		BEQ		three
 		b		thrnext
-three	ldr		r4, =number3
+three		ldr		r4, =number3
 
-thrnext	cmp		r6, #4
+thrnext		cmp		r6, #4
 		BEQ		four
 		b		fonext
-four	ldr		r4, =number4
+four		ldr		r4, =number4
 
-fonext	cmp		r6, #5
+fonext		cmp		r6, #5
 		BEQ		five
 		b		print
-five	ldr		r4, =number5
+five		ldr		r4, =number5
 		
 			
-print	movs	r1, #0			
-		movs	r2, #0		
-		ldr		r1, [r7, #4]	;row counter
+print		movs		r1, #0			
+		movs		r2, #0		
+		ldr		r1, [r7, #4]		;row counter
 row		str		r1, [r0]		;update the row register
 
-		ldr		r2, [r7, #8]	;column counter
+		ldr		r2, [r7, #8]		;column counter
 
-column	str		r2, [r0, #0x4]	;update column register			
-		ldr 	r3, [r4]		
-		str		r3, [r0, #0x8]	;write color to pixel
-		adds	r4, r4, #4		;next pixel color
-		adds	r2, r2, #1		;next column
+column		str		r2, [r0, #0x4]		;update column register			
+		ldr 		r3, [r4]		
+		str		r3, [r0, #0x8]		;write color to pixel
+		adds		r4, r4, #4		;next pixel color
+		adds		r2, r2, #1		;next column
 		ldr		r6, =15	
 		ldr		r5, [r7, #8]
-		adds	r6, r6, r5
+		adds		r6, r6, r5
 		cmp		r2, r6			;check if it reached end of column of image
-		bne 	column
-		adds	r1, r1, #1
+		bne 		column
+		adds		r1, r1, #1
 		ldr		r6, =25
 		ldr		r5, [r7, #4]
-		adds	r6, r6, r5
+		adds		r6, r6, r5
 		cmp		r1, r6			;check if it reached end of row of image
 		bne		row
 		
@@ -392,8 +391,8 @@ column	str		r2, [r0, #0x4]	;update column register
 		ENDP
 ;--------------------------------------------------------------------------------
 		;goal function to find whether it is goal or not
-isgoal	PROC
-		PUSH	{LR}			;saving lr for another subroutine
+isgoal		PROC
+		PUSH		{LR}			;saving lr for another subroutine
 		ldr		r2, =0			;reseting goal register
 		
 		ldr		r7, =ballcor
@@ -409,9 +408,9 @@ isgoal	PROC
 		BX 		LR
 		
 		;writing goal 
-p2goal	ldr		r7, =p2scr
+p2goal		ldr		r7, =p2scr
 		ldr		r6, [r7]
-		adds	r6, r6, #1		;adding goal
+		adds		r6, r6, #1		;adding goal
 		str		r6, [r7]		;updating goal
 		cmp		r6, #5			;check if player wins
 		BNE		out
@@ -424,9 +423,9 @@ p2goal	ldr		r7, =p2scr
 		b		win
 		
 		;writing goal
-p1goal	ldr		r7, =p1scr
+p1goal		ldr		r7, =p1scr
 		ldr		r6, [r7]
-		adds	r6, r6, #1		;adding goal
+		adds		r6, r6, #1		;adding goal
 		str		r6, [r7]		;updating goal
 		cmp		r6, #5			;check if player wins
 		BNE		out
@@ -453,34 +452,34 @@ win		ldr		r1, =p
 		ldr		r2, =135
 		ldr		r3, =85
 		BL		final
-		movs	r1, #1
-		str		r1, [r0, #0xc]	;refresh lcd
-		movs	r1, #2
-		str		r1, [r0, #0xc]	;clear lcd buffer
+		movs		r1, #1
+		str		r1, [r0, #0xc]		;refresh lcd
+		movs		r1, #2
+		str		r1, [r0, #0xc]		;clear lcd buffer
 		b		stop
 		
 		ENDP
 ;----------------------------------------------------------------------
 		;final drawing for the winner
-final	PROC
-		PUSH	{LR}			;saving lr for subroutine
+final		PROC
+		PUSH		{LR}			;saving lr for subroutine
 		ldr		r4, =124		;row counter
 		
-finrow	str		r4, [r0]		;update the row register
+finrow		str		r4, [r0]		;update the row register
 
 		mov		r5, r2			;column counter
 
-fincol	str		r5, [r0, #0x4]	;update column register			
-		ldr 	r6, [r1]		
-		str		r6, [r0, #0x8]	;write color to pixel
-		adds	r1, r1, #4
-		adds	r5, r5, #1		;next column
+fincol		str		r5, [r0, #0x4]		;update column register			
+		ldr 		r6, [r1]		
+		str		r6, [r0, #0x8]		;write color to pixel
+		adds		r1, r1, #4
+		adds		r5, r5, #1		;next column
 		ldr		r6, =0
-		adds	r6, r6, r2
-		adds	r6, r6, r3
+		adds		r6, r6, r2
+		adds		r6, r6, r3
 		cmp		r5, r6			;check if it reached end of column of image
-		bne 	fincol
-		adds	r4, r4, #1
+		bne 		fincol
+		adds		r4, r4, #1
 		ldr		r6, =149
 		cmp		r4, r6			;check if it reached end of row of image
 		bne		finrow	
@@ -491,14 +490,14 @@ fincol	str		r5, [r0, #0x4]	;update column register
 		ENDP
 
 		AREA	myData, DATA, READWRITE
-		EXPORT	p1cor
-		EXPORT	p2cor	
-p1scr	DCD		0, 5, 135		;score of player 1
-p2scr	DCD		0, 5, 170		;score of player 2
-balldir	DCD		1, -1			;moving direction of the ball
-ballcor	DCD		132, 156		;coordinates of the ball
-p1cor	DCD		121				;coordinates of player 1
-p2cor	DCD		121				;coordinates of player 2
+		EXPORT		p1cor
+		EXPORT		p2cor	
+p1scr		DCD		0, 5, 135		;score of player 1
+p2scr		DCD		0, 5, 170		;score of player 2
+balldir		DCD		1, -1			;moving direction of the ball
+ballcor		DCD		132, 156		;coordinates of the ball
+p1cor		DCD		121			;coordinates of player 1
+p2cor		DCD		121			;coordinates of player 2
 
 		END
 
